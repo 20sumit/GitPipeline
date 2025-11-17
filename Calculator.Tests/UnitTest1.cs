6a3aae13831af124b069e5ac8418cd8424fd64ad
Sumit Giri <sumitg@prescient.in>
2025-11-17 12:23:21 +0530

    added both project in git

---
+++ Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using Calculator;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectValue()
+        {
+            Assert.Equal(8, CalculatorService.Add(5, 3));
+        }
+
+        [Fact]
+        public void Sub_ReturnsCorrectValue()
+        {
+            Assert.Equal(2, CalculatorService.Sub(5, 3));
+        }
+
+        [Fact]
+        public void Mul_ReturnsCorrectValue()
+        {
+            Assert.Equal(15, CalculatorService.Mul(3, 5));
+        }
+
+        [Fact]
+        public void Div_ReturnsCorrectValue()
+        {
+            Assert.Equal(2.5, CalculatorService.Div(5, 2));
+        }
+
+        [Fact]
+        public void Div_ThrowsWhenDividingByZero()
+        {
+            Assert.Throws<DivideByZeroException>(() => CalculatorService.Div(5, 0));
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class CalculatorService
+    {
+        public static int Add(int a, int b) => a + b;
+        public static int Sub(int a, int b) => a - b;
+        public static int Mul(int a, int b) => a * b;
+        public static double Div(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return (double)a / b;
+        }
+    }
+}
